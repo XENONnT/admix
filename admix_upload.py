64649c4ffc5793aa616f8db11ab17c2171512cfa
Evan Shockley <ershockley@uchicago.edu>
2020-03-21 16:58:49 +0100

    Remove old config stuff from admix_upload, add function to delete rules

--- admix_upload.py
+++ admix_upload.py
@@ -4,6 +4,7 @@ from admix.interfaces.rucio_dataformat import ConfigRucioDataFormat
 from admix.interfaces.rucio_summoner import RucioSummoner
 from admix.interfaces.keyword import Keyword
 from admix.interfaces.database import ConnectMongoDB
+from admix.utils import make_did
 
 DB = ConnectMongoDB()
 
@@ -45,17 +46,17 @@ def find_data_to_upload():
 
 
 def do_upload(periodic_check=300):
-    rc_reader_path = "/home/datamanager/software/admix/admix/config/xenonnt_format.config"
-    rc_reader = ConfigRucioDataFormat()
-    rc_reader.Config(rc_reader_path)
+    #rc_reader_path = "/home/datamanager/software/admix/admix/config/xenonnt_format.config"
+    #rc_reader = ConfigRucioDataFormat()
+    #rc_reader.Config(rc_reader_path)
 
     rc = RucioSummoner()
 
     # get the data to upload
     ids_to_upload = find_data_to_upload()
 
-    cursor = DB.db.find({'_id': {"$in": ids_to_upload}
-                          #'number': 7158}
+    cursor = DB.db.find({'_id': {"$in": ids_to_upload},
+                         #'number': 7157
                          },
                         {'number': 1, 'data': 1, 'dids': 1})
 
@@ -87,35 +88,32 @@ def do_upload(periodic_check=300):
             file = datum['location'].split('/')[-1]
 
             #Init a class to handle keyword strings:
-            keyw = Keyword()
+            #keyw = Keyword()
             hash = file.split('-')[-1]
             # Apparently the GetPlugin method is stateful. It will remember previous run information
             # if you don't pass reset=True... #$%#$W^$
-            rucio_template = rc_reader.GetPlugin(dtype, reset=True)
+            #rucio_template = rc_reader.GetPlugin(dtype, reset=True)
             upload_path = os.path.join(DATADIR, file)
-            print(file)
 
-            keyw.SetTemplate({'hash': hash, 'plugin': dtype, 'number':'%06d' % number})
+            #keyw.SetTemplate({'hash': hash, 'plugin': dtype, 'number':'%06d' % number})
 
-            rucio_template = keyw.CompleteTemplate(rucio_template)
+            #rucio_template = keyw.CompleteTemplate(rucio_template)
 
-            rucio_template_sorted = [key for key in sorted(rucio_template.keys())]
+            #rucio_template_sorted = [key for key in sorted(rucio_template.keys())]
 
+            did = make_did(number, dtype, hash)
 
-            rucio_rule = rc.GetRule(upload_structure=rucio_template, rse="LNGS_USERDISK")
+            rucio_rule = rc.GetRule(upload_structure=did, rse="LNGS_USERDISK")
 
-            print(rucio_rule)
             if not in_rucio and not rucio_rule['exists']:
-                result = rc.Upload(upload_structure=rucio_template,
-                                   upload_path=upload_path,
-                                   rse='LNGS_USERDISK',
-                                   rse_lifetime=None)
+                result = rc.Upload(did,
+                                   upload_path,
+                                   'LNGS_USERDISK',
+                                   lifetime=None)
 
                 print("Dataset uploaded")
             # if upload was successful, tell runDB
-            rucio_rule = rc.GetRule(upload_structure=rucio_template, rse="LNGS_USERDISK")
-            did = rucio_template[rucio_template_sorted[-1]]['did']
-            print(did)
+            rucio_rule = rc.GetRule(upload_structure=did, rse="LNGS_USERDISK")
             data_dict = {'host': "rucio-catalogue",
                          'type': dtype,
                          'location': 'LNGS_USERDISK',
@@ -125,8 +123,9 @@ def do_upload(periodic_check=300):
                          'protocol': 'rucio'
                          }
 
-            if not in_rucio and rucio_rule['state'] == 'OK':
-                DB.AddDatafield(run['_id'], data_dict)
+            if rucio_rule['state'] == 'OK':
+                if not in_rucio:
+                    DB.AddDatafield(run['_id'], data_dict)
 
                 # add a DID list?
                 # check if did field exists yet or not
@@ -147,7 +146,6 @@ def do_upload(periodic_check=300):
         if time.time() - last_check > periodic_check:
             check_transfers()
             last_check = time.time()
-
 
 
 def add_rule(run_number, dtype, rse, lifetime=None, update_db=True):
@@ -181,9 +179,12 @@ def check_transfers():
 
     rc = RucioSummoner()
 
-    print("Checking transfer status of %d runs" % len(list(cursor)))
+    cursor = list(cursor)
 
-    for run in cursor:
+    print("Checking transfer status of %d runs" % len(cursor))
+
+
+    for run in list(cursor):
         # for each run, check the status of all REPLICATING rules
         rucio_stati = []
         for d in run['data']:
@@ -192,11 +193,12 @@ def check_transfers():
                     rucio_stati.append(d['status'])
                 else:
                     did = d['did']
-                    status = rc.GetRule(did, d['location'])
+                    status = rc.CheckRule(did, d['location'])
                     if status == 'REPLICATING':
                         rucio_stati.append('transferring')
                     elif status == 'OK':
                         # update database
+                        print("Updating DB for run %d, dtype %s" % (run['number'], d['type']))
                         DB.db.find_one_and_update({'_id': run['_id'],'data': {'$elemMatch': d}},
                                                   {'$set': {'data.$.status': 'transferred'}}
                                                   )
@@ -208,15 +210,14 @@ def check_transfers():
                                                   )
                         rucio_stati.append('error')
 
-
         # are there any other rucio rules transferring?
-        if all([s == 'transferred' for s in rucio_stati]):
+        if len(rucio_stati) > 0 and all([s == 'transferred' for s in rucio_stati]):
             set_status(run['_id'], 'transferred')
 
 
 def clear_db():
-    dtypes = ['raw_records', 'raw_records_mv', 'raw_records_aqmon', 'raw_records_lowgain']
-    numbers = [7159, 7160, 7161]
+    dtypes = ['raw_records'] #, 'raw_records_mv', 'raw_records_aqmon', 'raw_records_lowgain']
+    numbers = [7157]
     for run in numbers:
         doc = DB.GetRunByNumber(run)[0]
         docid = doc['_id']
@@ -229,17 +230,84 @@ def clear_db():
         set_status(docid, 'needs_upload')
 
 
+def purge():
+    query = {'data': {"$elemMatch": {'location': "LNGS_USERDISK"}}}
+    cursor = list(DB.db.find(query, {'number': 1, 'data': 1}))
+
+    rc = RucioSummoner()
+
+    print("Checking %d runs if they can be purged from LNGS" % len(cursor))
+    for run in cursor:
+        # get datatypes that are still at LNGS, according to runDB
+        dtypes = set()
+        for d in run['data']:
+            if d['host'] == 'rucio-catalogue' and d['location'] == 'LNGS_USERDISK':
+                dtypes.add(d['type'])
+
+        # now loop over the LNGS dtypes to see if we can purge anything
+        # require 2 rucio copies outside LNGS to purge
+        lngs_datum = None
+        for dtype in dtypes:
+            rucio_copies = 0
+            for d in run['data']:
+                if (d['host'] == 'rucio-catalogue'
+                    and d['type'] == dtype
+                    and d['status'] == 'transferred'
+                    ):
+
+                    if d['location'] == 'LNGS_USERDISK':
+                        lngs_datum = d
+                    else:
+                        rucio_copies += 1
+
+
+
+            if rucio_copies >= 2:
+                # remove the non-rucio copy in /eb
+                #remove_from_eb(run['number'], dtype)
+                print("=== REMOVE FROM EB ===")
+
+                # remove the LNGS rule
+                did = get_did(run['number'], dtype)
+                scope, dset = did.split(':')
+                rules = rc._rucio.ListDidRules(scope, dset)
+                # get the rule id
+                rule_id = None
+                for r in rules:
+                    if r['rse_expression'] == 'LNGS_USERDISK':
+                        rule_id = r['id']
+
+                if rule_id is None:
+                    print("No LNGS rule found for run %d %s" % (run['number'], dtype))
+
+                else:
+                    print("Deleting LNGS rule for %d %s" % (run['number'], dtype))
+                    DB.RemoveDatafield(run['_id'], lngs_datum)
+                    rc.DeleteRule(rule_id)
+        break
+
+
+def remove_from_eb(number, dtype):
+    query = {'number': number}
+    cursor = list(DB.db.find(query, {'number': 1, 'data': 1}))
+
+
+
+
 def main():
     while True:
         find_new_data()
         print("Starting uploads")
         do_upload()
         print("Sleeping...\n")
-        #time.sleep(120)
+        #time.sleep(300)
         break
 
 
 
 if __name__ == "__main__":
-    main()
-    #clear_db()
+    #main()
+    # clear_db()
+    purge()
+    #check_transfers()
+
