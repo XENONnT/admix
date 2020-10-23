a03f39240a6efa5b2b4c0fe6225ec4fc3728634f
Evan Shockley <ershockley@uchicago.edu>
2020-10-23 10:04:21 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = ['pymongo',
-                'utilix'
+                'git+git://github.com/XENONnT/utilix'
                 ]
 
 setup_requirements = [
