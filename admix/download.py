bb9685f61ac4c9f45bbb11eb0d4296af7ac4941b
Evan Shockley <ershockley@uchicago.edu>
2020-11-10 07:41:42 -0800

    Update download.py
    Remove legacy code checking if data was on US endpoint.

--- admix/download.py
+++ admix/download.py
@@ -22,14 +22,9 @@ def determine_rse(rse_list, glidein_country):
 
 
     if glidein_country == "US":
-        in_US = False
         for site in US_SITES:
             if site in rse_list:
                 return site
-
-        if not in_US:
-            print("This run is not in the US so can't be processed here. Exit 255")
-            sys.exit(255)
 
     elif glidein_country == "FR":
         for site in EURO_SITES:
