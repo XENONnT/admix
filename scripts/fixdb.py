133af87a27b66a069f00a7a3039e92cf2dccacf1
Luca Scotto Lavina <scotto@lpnhe.in2p3.fr>
2020-11-17 16:25:49 +0100

    New utiliy in fixdb: it is capable to completely fix a failed upload without manual intervention

--- scripts/fixdb.py
+++ scripts/fixdb.py
@@ -156,6 +156,134 @@ def remove_datatype_from_db_and_datamanager(did):
         os.remove(file)
 
 
+def reset_upload(did):
+
+    config = Config()
+    helper.make_global("admix_config", os.path.abspath(config.get('Admix','config_file')))
+    RSES = helper.get_hostconfig()['rses']
+
+
+    DB = ConnectMongoDB()
+    rc = RucioSummoner()
+
+    hash = did.split('-')[-1]
+    dtype = did.split('-')[0].split(':')[-1]
+    number = int(did.split(':')[0].split('_')[-1])
+
+    print("Resetting the upload associated to the DID: {0}".format(did))
+    print("Run number: {0}".format(number))
+    print("Data type: {0}".format(dtype))
+    print("Hash: {0}".format(hash))
+
+    run = DB.db.find_one({'number' : number})
+
+    # Gets the status
+    if 'status' in run:
+        print('Run status: {0}'.format(run['status']))
+    else:
+        print('Run status: {0}'.format('Not available'))
+
+    # Extracts the correct Event Builder machine who processed this run
+    # Then also the bootstrax state and, in case it was abandoned, the reason
+    if 'bootstrax' in run:
+        bootstrax = run['bootstrax']
+        eb = bootstrax['host'].split('.')[0]
+    else:
+        print('Not processed')
+        return(0)
+
+
+    # Get the EB datum and its status
+    ebstatus = ""
+    datum = None
+    for d in run['data']:
+        if d['type'] == dtype and eb in d['host']:
+            datum = d
+            if 'status' in d:
+                ebstatus = d['status']
+
+    if datum is None:
+        print('There is no EB datum. No reset is possible')
+        return(0)
+
+    if ebstatus != "":
+        print('EB status: {0}'.format(ebstatus))
+    else:
+        print('EB status: not available')
+
+
+
+
+
+    # Step zero (normally not needed): change the run status to "transferring"
+    #    DB.db.find_one_and_update({'number':number},{'$set':{"status": "transferring"}})
+
+
+    # First action: remove the files stored in datamanager
+    files = list_file_replicas(number, dtype, hash, "LNGS_USERDISK")
+    print("Deleting rucio data in datamanager disk. Deleting",len(files),"files")
+    for file in files:
+        os.remove(file)
+
+
+
+    # Second action: remove the LNGS Rucio rule
+    rucio_rule = rc.GetRule(upload_structure=did, rse='LNGS_USERDISK')
+    if rucio_rule['exists']:
+        print("Deleting rucio rule = ", rucio_rule['id'])
+        rc.DeleteRule(rucio_rule['id'])
+        # Wait for 1 hour (plus 5 minutes of margin)
+        delay = 3600+60*5
+        print("We have to wait for {0} seconds before proceeding to the next step".format(delay))
+        time.sleep(delay)
+    else:
+        print("There is no rule to delete")
+
+
+
+    # Third action: set the EB status as 'eb_ready_to_upload' 
+    DB.db.find_one_and_update({'_id': run['_id'],'data': {'$elemMatch': datum}},
+                                   {'$set': {'data.$.status': 'eb_ready_to_upload'}})
+    print("EB status changed to eb_ready_to_upload")
+
+
+
+    # Reload the run
+    run = DB.db.find_one({'number' : number})
+
+    # Gets the status
+    if 'status' in run:
+        print('New run status: {0}'.format(run['status']))
+    else:
+        print('Ru status: {0}'.format('Not available'))
+
+    # Get the EB datum and its status
+    ebstatus = ""
+    datum = None
+    for d in run['data']:
+        if d['type'] == dtype and eb in d['host']:
+            datum = d
+            if 'status' in d:
+                ebstatus = d['status']
+
+    # Prints the eb status as a confirmation of the performed change 
+    if ebstatus != "":
+        print('New EB status: {0}'.format(ebstatus))
+    else:
+        print('New EB status: not available')
+
+
+    # Remove the LNGS DB entry
+#    for d in run['data']:
+#        if d['type'] == dtype and d['host'] == 'rucio-catalogue' and d['location'] == 'LNGS_USERDISK':
+#            # Remove the data entry in DB
+#            print("Deleting data = ",d)
+#            DB.db.update_one({"_id" : run['_id']},
+#                              {"$pull" : {"data" : d} })
+
+
+
+
 
 def change_status(number,status):
 
@@ -219,8 +347,19 @@ if __name__ == "__main__":
 #    remove_datatype_from_db_and_datamanager("xnt_008650:lone_hits-b7dgmtzaef")
 #    remove_datatype_from_db_and_datamanager("xnt_008013:raw_records-rfzvpzj4mf")
 #    remove_datatype_from_db_and_datamanager("xnt_008620:records-jxkqp76kam")
-#    change_status(9492,"eb_ready_to_upload")
-    change_status(9492,"transferred")
+#    change_status(10090,"eb_ready_to_upload")
+#    change_status(10088,"eb_ready_to_upload")
+#    change_status(10089,"eb_ready_to_upload")
+#    change_status(10092,"eb_ready_to_upload")
+#    change_status(10093,"eb_ready_to_upload")
+#    change_status(10066,"eb_ready_to_upload")
+
+#    reset_upload("xnt_010215:raw_records_he-rfzvpzj4mf")
+    reset_upload("xnt_010277:peaklets_he-2bwh4goml5")
+#    change_status(9678,"eb_ready_to_upload")
+#    change_status(9614,"eb_ready_to_upload")
+#    change_status(9615,"eb_ready_to_upload")
+#    change_status(9492,"transferred")
 #    change_status(8896,"uploading")
 #    change_status(7151,"")
 #    fixdb(8650)
