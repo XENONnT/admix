f5320b7a128967427928dcb4896859bda1567090
Evan Shockley <shockley.evan@gmail.com>
2020-12-17 10:26:33 -0800

    New rucio naming scheme

--- admix/utils/naming.py
+++ admix/utils/naming.py
@@ -1,7 +1,13 @@
 
 
-
 def make_did(run_number, dtype, hash):
+    # batch scopes into bunches of 10k
+    scope = "xnt_%02d" % (run_number / 10000)
+    dataset = "%06d-%s-%s" % (run_number, dtype, hash)
+    return "%s:%s" % (scope, dataset)
+
+
+def make_did_old(run_number, dtype, hash):
     scope = 'xnt_%06d' % run_number
     dataset = "%s-%s" % (dtype, hash)
     return "%s:%s" % (scope, dataset)
