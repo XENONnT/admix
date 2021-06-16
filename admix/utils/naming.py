3b987de6d4682a007b3d7d4f585caedb769792e1
Luca Scotto Lavina <scotto@lpnhe.in2p3.fr>
2021-06-16 22:27:52 +0200

    Added get_git that gives you run number, data type and hash for a given did

--- admix/utils/naming.py
+++ admix/utils/naming.py
@@ -6,3 +6,10 @@ def make_did(run_number, dtype, hash):
     dataset = "%s-%s" % (dtype, hash)
     return "%s:%s" % (scope, dataset)
 
+def get_did(did):
+    scope, dataset = did.split(':')
+    number = int(scope.split('_')[-1])
+    dtype = dataset.split('-')[0]
+    hash = dataset.split('-')[-1]
+    return number, dtype, hash
+
